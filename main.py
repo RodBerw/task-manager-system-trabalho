f43389b19759029dbd4c6cf0639741030314523e
Guilherme <guiguigo4444@gmail.com>
2024-09-11 19:07:02 -0300

    test

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-from flask import render_template, redirect, url_for, request
-from config import app, db
-from models import User, Task
-=======
 # from flask import render_template, redirect, url_for, request
 # from config import app
 # from models import User, Task
->>>>>>> 3ebb4824dbcd913a0910b3f86f1699c53ce8add5
 
 # @app.route("/")
 # def home():
@@ -16,19 +10,10 @@ from models import User, Task
 # def register():
 #     return render_template('register.html')
 
-<<<<<<< HEAD
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
-with app.app_context():
-    db.create_all()
-=======
 # @app.route("/login", methods=['GET', 'POST'])
 # def login():
 #     value = request.args.get('value')
 #     return render_template('login.html', value=value)
->>>>>>> 3ebb4824dbcd913a0910b3f86f1699c53ce8add5
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
